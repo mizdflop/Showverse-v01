00f0946514a86e805410dae81974e1febd06bb75
Jay Greenspan <jayhuntsfish@gmail.com>
2014-04-12 22:16:41 -0400

    move subs to its own file

---
+++ server/subscriptions.js
@@ -0,0 +1,73 @@
+Meteor.publish('comments', function (idString, seriesTitle, groupName) {
+    console.log(idString);
+  return Comments.find({idString: idString, seriesTitle: seriesTitle, groupName: groupName});
+});
+Meteor.publish('groups', function (groupName) {
+  return Groups.find({groupName: groupName});
+});
+Meteor.publish('series', function(seriesTitle) {
+    return Series.find({seriesTitle: seriesTitle});
+}); 
+Meteor.publish('episodes_for_list', function(seriesTitle) {
+    console.log(seriesTitle);
+    return Episodes.find({seriesTitle: seriesTitle});
+}); 
+Meteor.publish('episodes', function(seriesTitle, idString) {
+    console.log(idString);
+    return Episodes.find({idString: idString, seriesTitle: seriesTitle});
+}); 
+Meteor.publish('admin_episodes', function() {
+    return Episodes.find();
+}); 
+Meteor.publish('admin_series', function() {
+    return Series.find();
+});     
+Meteor.publish('commentsmeta', function(idString, groupName) {
+    return CommentsMeta.find({idString: idString, groupName: groupName});
+}); 
+Meteor.publish('commentedUsers', function(){
+    console.log("this is me here.");
+    return Meteor.users.find({});
+});
+serverCommentsPerEpisode = new Meteor.Collection("servercommentsperepisode");
+
+//working agg function
+//db.comments.aggregate(  {$group: {_id: { seriesTitle: '$seriesTitle', seasonNumber: '$seasonNumber', episodeNumber: '$episodeNumber' }, posts: {$sum: 1}}}   )
+Meteor.publish("commentsperepisode", function () {
+    var sub = this;
+    // This works for Meteor 0.6.5
+    var db = MongoInternals.defaultRemoteCollectionDriver().mongo.db;
+
+    // Your arguments to Mongo's aggregation. Make these however you want.
+    var pipeline = [
+        //{ $match: doSomethingWith(args) },
+        { $group: {
+            _id: { seriesTitle: '$seriesTitle', seasonNumber: '$seasonNumber', episodeNumber: '$episodeNumber' },
+            count: { $sum: 1 }
+        }}
+    ];
+    console.log(pipeline);
+
+    db.collection("ServerCommentsPerEpisode").aggregate(        
+        pipeline,
+        // Need to wrap the callback so it gets called in a Fiber.
+        Meteor.bindEnvironment(
+            function(err, result) {
+                console.log(result);
+                // Add each of the results to the subscription.
+                _.each(result, function(e) {
+                    // Generate a random disposable id for aggregated documents
+                    sub.added("CommentsPerEpisode", Random.id(), {
+                        key: e._id.somethingOfInterest,                        
+                        count: e.count
+                    });
+                });
+
+                sub.ready();
+            },
+            function(error) {
+                Meteor._debug( "Error doing aggregation: " + error);
+            }
+        )
+    );
+});
